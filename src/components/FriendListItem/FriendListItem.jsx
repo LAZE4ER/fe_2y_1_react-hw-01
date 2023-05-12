dd8fdfc03bf61574f509c2c88e3f87a861652f5d
LAZE4ER <skripecdana@gmail.com>
2023-05-12 21:24:02 +0300

    final

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,12 @@
-import FriendList from '../FriendList/FriendList'
+import styles from './FriendListItem.module.css'
 const FriendListItem =  ({ name, avatar, isOnline, id  }) => { 
     return(
     <li class="item">
-    <span class="status">{isOnline}</span>
+    <span  className={isOnline ? styles.online : styles.offline} >{isOnline}  </span>
     <img class="avatar" src={avatar} alt={id} width="48"  />
     <p class="name">{name}</p>
   </li>
+  
   )}
 
   export default FriendListItem;
